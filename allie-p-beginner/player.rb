41fd66df676d646d7a5e1c44257f8032ac5a1bd8
Allison <asheren@neighborsations.com>
2013-11-18 20:43:18 -0500

    finished level three

--- allie-p-beginner/player.rb
+++ allie-p-beginner/player.rb
@@ -2,8 +2,13 @@ class Player
   def play_turn(warrior)
     warrior.feel
     if warrior.feel.empty?
-    	warrior.walk!
+    	if warrior.health < 14
+    	  warrior.rest!
+    	else
+    	  warrior.walk!
+    	end
     else
+    	warrior.feel.enemy?
     	warrior.attack!
     end
   end
